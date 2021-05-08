d529fea56daea69b34b0ffdb5c36fbf007be34fb
sibiya666 <sc2tvsibiya@gmail.com>
2021-05-08 07:47:09 +0300

    add ngxs

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        NgxsModule.forRoot(),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
